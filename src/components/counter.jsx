76bd7f90ed33ba6b59d2366ee533cbdfb10c744c
Terrance Lewis <terrance@Jordans-MacBook-Pro.local>
2020-07-17 17:47:17 -0400

    init

---
+++ src/components/counter.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
+    }
+
+    styles = {
+        fontSize: 15,
+        fontWeight: 'bold'
+    }
+    render() { 
+        return (
+        <div className="container m-3">
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-small" onClick={() =>this.incrementCount()}>Increment</button>
+        </div>
+        )   
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    incrementCount() {
+        this.setState({ count: this.state.count + 1})
+        console.log('test')
+    }
+
+    formatCount() {
+        const { count } = this.state
+        return count === 0 ? 'Zero' : count;
+    }
+}
+ 
+export default Counter;
---
+++ src/components/resetButton.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ResetButton extends Component {
+    render() { 
+        return ( 
+            <button className="btn">Reset</button>
+         );
+    }
+}
+ 
+export default ResetButton;
